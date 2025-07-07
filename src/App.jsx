8e4b714f4c4c3bd6b5fc3d3a2a081b81ee23fefa
Alejandro Baran <baran.alejandro04@gmail.com>
2025-07-07 09:04:33 -0300

    tarea terminada

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ColorCardPaletteList from './Components/ColorCardPaletteList/ColorCardPaletteList'
+
+function App() {
+
+  return (
+    <div className='div-app'>
+      <ColorCardPaletteList colors={[
+        {likes: 9, time: '2 days', colors: ['#1B3C53', '#456882','#D2C1B6', '#F9F3EF']}, 
+        {likes: 9, time: '14 minutes', colors: ['#748873', '#D1A980','#E5E0D8', '#F8F8F8']}, 
+        {likes: 9, time: '5 hours', colors: ['#5EABD6', '#FEFBC7','#FFB4B4', '#E14434']}, 
+        ]}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ColorCardPaletteList/ColorCardPaletteList.jsx
@@ -0,0 +1,66 @@
+
+import React, {useState} from "react";
+import "./ColorCardPaletteList.css"
+
+const ColorCardPaletteList = ({colors}) => {
+    return(
+        <>
+            {colors.map((color) => {
+                return (
+                    <div>
+                        <ColorCardPalette likes={color.likes} time={color.time} colors={color.colors} />
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+const ColorCardPalette = ({likes: initialLikes, time, colors}) => {
+    const [likes, setLikes] = useState(initialLikes)
+    const [isLiked, setIsLiked] = useState(false)
+
+    return(
+        <div className="color-card">
+            <div className="color-card-palette">
+                <div className="color-0" style={{background: colors[0]}}>
+                    <span className="color-text">
+                        {colors[0]}   
+                    </span>
+                </div>
+                <div className="color-1" style={{background: colors[1]}}>
+                    <span className="color-text">
+                        {colors[1]}   
+                    </span>
+                </div>
+                <div className="color-2" style={{background: colors[2]}}>
+                    <span className="color-text">
+                        {colors[2]}   
+                    </span>
+                </div>
+                <div className="color-3" style={{background: colors[3]}}>
+                    <span className="color-text">
+                        {colors[3]}   
+                    </span>
+                </div>
+            </div>
+            <div className="color-card-footer">
+                <button className="button-likes" onClick={() => {
+                    if (isLiked) {
+                        setLikes(likes - 1)
+                    } 
+                    else {
+                        setLikes(likes + 1)
+                    }
+                    setIsLiked(!isLiked)
+                }}>
+                    <i className={`bi ${isLiked ? "bi-heart-fill" : "bi-heart"}`} style={{ color: isLiked ? "red" : "black" }}></i>
+                    <div className="color-card-likes">{likes}</div>
+                </button>
+                <span className="color-card-time">{time}</span>
+            </div>
+        </div>
+    )
+}
+
+export default ColorCardPaletteList
